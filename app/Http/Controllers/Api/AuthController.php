a84185196920cffc652bdc6bb7340ab3aa6dae4b
jordan <jordan.raphael@outlook.fr>
2023-09-01 00:09:39 +0200

    Sign up login logout with user info

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,80 +2,50 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
-use Illuminate\Foundation\Auth\User;
+use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function signup(SignupRequest $request) {
-        $data = $request->validate([
-            'name' => 'required|string|max:255',
-            'first_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'phone_number' => 'required|string|max:20',
-            'address' => 'required|string|max:255',
-            'city' => 'required|string|max:255',
-            'postal_code' => 'required|string|max:20',
-            'siret_number' => 'nullable|string|max:20',
-            'available_space' => 'nullable|numeric',
-            'password' => 'required|string|min:8|confirmed'
-        ]);
-
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+        /** @var \App\Models\User $user */
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => $data['password'],
+            'password' => bcrypt($data['password']),
         ]);
-
-        $token  = $user->createToken('main')->plainTextToken;
-
-        return response([
-            'user' => $user,
-            'token' =>$token
-        ]);
-    }
-
-    public function login(LoginRequest $request) {
-
-        $credentials = $request->validate([
-            'name' => 'required|string|max:255',
-            'first_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'phone_number' => 'required|string|max:20',
-            'address' => 'required|string|max:255',
-            'city' => 'required|string|max:255',
-            'postal_code' => 'required|string|max:20',
-            'siret_number' => 'nullable|string|max:20',
-            'available_space' => 'nullable|numeric',
-            'password' => 'required|string|min:8|confirmed'
-        ]);
-
-        if (Auth::attempt([$credentials])) {
-            return response([
-                'Message' =>   'Email ou mot de passe incorrect',
-            ], 204);
-        }
-
-        $user = Auth::user();
 
         $token = $user->createToken('main')->plainTextToken;
-
-        return response([
-            'user' => $user,
-            'token' => $token
-        ], 200);
-
+        return response(compact('user', 'token'));
     }
-    public function logout(Request $request) {
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email or password is incorrect'
+            ], 422);
+        }
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request)
+    {
+        /** @var \App\Models\User $user */
         $user = $request->user();
-
-        $user->currentAccesToken()->delete();
-
-        return response('Utilisateur bien déconnecté', 204);
-    
+        $user->currentAccessToken()->delete();
+        return response('Utilisateur déconnecté', 204);
     }
 }
