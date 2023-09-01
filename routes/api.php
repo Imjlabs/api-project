c1e15a37cd58fd5e23ab163d434708907d7e4f99
jordan <jordan.raphael@outlook.fr>
2023-09-01 13:16:54 +0200

    Adding file create, list file and delete file

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\FileController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::apiResource('/users', UserController::class);
+
+    Route::post('/upload-file', [FileController::class, 'uploadFile']);
+
+    // Route pour supprimer un fichier par ID
+    Route::delete('/delete-file/{fileId}', [FileController::class, 'deleteFile']);
+
+    // Route pour obtenir la liste des fichiers de l'utilisateur connecté
+    Route::get('/list-files', [FileController::class, 'listFiles']);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
