4d3c4274433ced3cbc6747ea570ffe01a4b8fd90
jordan <jordan.raphael@outlook.fr>
2023-09-05 21:43:30 +0200

    Add storage to list user

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -21,8 +21,45 @@ class AdminController extends Controller
     {
 
         $users = User::where('role', 'user')->get();
+        $allFiles = [];
+        $totalFileCount = 0;
+        $totalFileSize = 0; // Taille totale des fichiers
+        $totalAvailableSpace = 0; // Taille disponible totale
+        $totalSpaceUsed = 0;
+        
+        foreach ($users as $user) {
+            $files = $user->files;
+            $fileCount = $files->count();
+            $totalFileCount += $fileCount;
+    
+            // Calculer la taille totale des fichiers pour cet utilisateur
+            $userTotalFileSize = $files->sum('file_size');
+            $totalFileSize += $userTotalFileSize;
+    
+            // Ajouter la taille disponible de cet utilisateur à la taille disponible totale
+            $totalAvailableSpace += $user->available_space;
+    
+            $allFiles[$user->id] = [
+                'user' => $user,
+                'files' => $files,
+                'file_count' => $fileCount,
+                'user_total_file_size' => $userTotalFileSize,
+            ];
+    
+            // Calculer l'espace utilisé par cet utilisateur
+            $spaceUsed = $userTotalFileSize / 1024; // Convertir en Mo
+            $totalSpaceUsed += $spaceUsed;
+        }
+    
+        // Calculer l'espace restant
+        $totalSpaceRemaining = $totalAvailableSpace - $totalSpaceUsed;
+    // Espace total utilisé
 
-        return response()->json(['users' => $users]);
+    return response()->json([
+        'allFiles' => $allFiles,
+        'totalFileCount' => $totalFileCount,
+        'totalFileSize' => $totalFileSize, // Espace restant
+    ], 200);
     }
 
     /**
